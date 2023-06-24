b74c13573ab9b4f9f5474aca81fb24350aca105a
diegogqcr <diego.gamboa.cr@gmail.com>
2023-06-24 12:13:35 -0600

    fix: search bar and button align

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -4,7 +4,7 @@ import styles from './Search.module.scss';
 
 const Search = ({ setSearch }) => {
     return (
-        <from className="d-flex justify-content-center gap-4 mb-5">
+        <from className="d-flex justify-content-center gap-4 mb-5 flex-sm-row flex-column align-items-center">
             <input
                 onChange={e => {
                     setSearch(e.target.value);
